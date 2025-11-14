ec19784ff78e8c9ecfdb37c4183969711371a3ee
sojiro takagi <soujirou.takagi@gmail.com>
2025-11-14 11:26:52 +0900

    #16 update:submitBtnにアイコン追加プロパティを追加

--- src/components/ui/SubmitBtn/SubmitBtn.tsx
+++ src/components/ui/SubmitBtn/SubmitBtn.tsx
@@ -1,13 +1,23 @@
 import styles from "@/components/ui/SubmitBtn/SubmitBtn.module.scss";
 import clsx from "clsx";
+import Image from "next/image";
 interface SubmitBtnWrap {
     text: string;
+    link?: boolean;
     onClick: () => void;
 }
 
-const SubmitBtn: React.FC<SubmitBtnWrap> = ({ text, onClick }) => {
+const SubmitBtn: React.FC<SubmitBtnWrap> = ({ text, link, onClick }) => {
     return (
         <button onClick={onClick} className={clsx(styles.btn_wrap, "text_xl")}>
+            {link && (
+                <Image
+                    src="/images/ui/link.svg"
+                    alt="リンクアイコン"
+                    width={26.67}
+                    height={13.33}
+                />
+            )}
             {text}
         </button>
     );
