803eddfb661d1b4f260c4c082b10b61a1312f77e
sojiro takagi <soujirou.takagi@gmail.com>
2025-11-17 01:08:08 +0900

    #16 update:image権限にremotePatternsを追加

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "picsum.photos",
+            },
+        ],
         domains: ["maps.googleapis.com", "lh3.googleusercontent.com", "maps.gstatic.com"],
     },
 };
