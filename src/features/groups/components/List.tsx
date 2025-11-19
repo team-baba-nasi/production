0191627b4d73cc11e5d781264b8afbe575e38f26
sojiro takagi <soujirou.takagi@gmail.com>
2025-11-19 19:33:24 +0900

    #16 fix:css_moduleにファイル名を変更

--- src/features/groups/components/List.tsx
+++ src/features/groups/components/List.tsx
@@ -1,23 +1,24 @@
 import Image from "next/image";
-import styles from "@/features/groups/styles/Group.module.scss";
+import styles from "@/features/groups/styles/List.module.scss";
 
 interface ListProps {
     icon?: string;
     name: string;
-    membersCount: number;
+    membersCount?: number;
 }
 
 const List: React.FC<ListProps> = ({ icon, name, membersCount }) => {
     return (
         <div className={styles.groupWrap}>
             <Image
-                src={icon ? `/images/groups/${icon}.webp` : "/images/groups/test_icon.webp"}
+                src={icon ? `${icon}` : "/images/groups/test_icon.webp"}
                 alt="groupIcon"
                 width={56}
                 height={56}
             />
             <p className="text_normal bold">
-                {name}({membersCount})
+                {name}
+                {membersCount && `(${membersCount})`}
             </p>
         </div>
     );
