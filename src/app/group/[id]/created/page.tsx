c22972e8df39919fc004cb0b502beb502b0d0d17
sojiro takagi <soujirou.takagi@gmail.com>
2025-11-23 04:27:20 +0900

    #16 update:hooks化に応じて修正

--- src/app/group/[id]/created/page.tsx
+++ src/app/group/[id]/created/page.tsx
@@ -7,38 +7,16 @@ import clsx from "clsx";
 import styles from "@/features/groups/styles/pages/GroupCreatedPage.module.scss";
 import { useGroupId } from "@/features/groups/hooks/useGroupId";
 import { useGroupFromId } from "@/features/groups/hooks/useGroupFromId";
-import { useCreateInviteToken } from "@/features/groups/hooks/useCreateInviteToken";
+import useGroupInviteURL from "@/features/groups/hooks/useGroupInviteURL";
 
 const GroupCreate = () => {
     const groupId = useGroupId();
-    const createInvite = useCreateInviteToken();
+    const copyURL = useGroupInviteURL();
 
     const { data, isLoading, error } = useGroupFromId(groupId);
 
     if (isLoading) return <p>読み込み中...</p>;
     if (error) return <p>エラー: {error.response?.data.error}</p>;
-
-    const handleCopyURL = () => {
-        createInvite.mutate(
-            { groupId },
-            {
-                onSuccess: async (data) => {
-                    try {
-                        await navigator.clipboard.writeText(data.inviteUrl);
-                        alert("招待URLをコピーしました！");
-                    } catch {
-                        alert(
-                            "URLのコピーに失敗しました…手動でコピーしてください。\n" +
-                                data.inviteUrl
-                        );
-                    }
-                },
-                onError: (err) => {
-                    alert(err.response?.data?.error ?? "エラーが発生しました");
-                },
-            }
-        );
-    };
 
     return (
         <div className={clsx("page_wrap", styles.pageWrap)}>
@@ -47,7 +25,7 @@ const GroupCreate = () => {
                 <GroupIcon img={`${data?.group.icon_image_url}`} size={100} />
                 <p className={clsx(styles.groupText, "text_normal bold")}>ババ抜きババ無し</p>
             </div>
-            <SubmitBtn text="リンクをコピー" onClick={handleCopyURL} link />
+            <SubmitBtn text="リンクをコピー" onClick={copyURL} link />
         </div>
     );
 };
