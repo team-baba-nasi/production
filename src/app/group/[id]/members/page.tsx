b651c2c66f4a1dc0386fb126f2b196a2723ee5ac
sojiro takagi <soujirou.takagi@gmail.com>
2025-11-23 14:50:00 +0900

    #16 update:ホスト追加処理をapi以外追加

--- src/app/group/[id]/members/page.tsx
+++ src/app/group/[id]/members/page.tsx
@@ -4,15 +4,25 @@ import GroupHeader from "@/features/groups/components/GroupHeader";
 import Label from "@/components/ui/Label/Label";
 import List from "@/features/groups/components/List";
 import styles from "@/features/groups/styles/pages/GroupMembersPage.module.scss";
-import GroupSettings from "@/features/groups/components/GroupSettings";
 import clsx from "clsx";
+import SubmitBtn from "@/components/ui/SubmitBtn/SubmitBtn";
 import { useGroupId } from "@/features/groups/hooks/useGroupId";
 import { useGroupMembersFromId } from "@/features/groups/hooks/useGroupMembersFromId";
+import { useState } from "react";
 
 const GroupMembers = () => {
     const groupId = useGroupId();
 
     const { data, isLoading, error } = useGroupMembersFromId(groupId);
+
+    const [addHost, setAddHost] = useState<boolean>(false);
+    const [selectedAddHost, setSelectedAddHost] = useState<number[]>([]);
+
+    const handleSelectAddHost = (id: number) => {
+        setSelectedAddHost((prev) =>
+            prev.includes(id) ? prev.filter((v) => v !== id) : [...prev, id]
+        );
+    };
 
     if (isLoading) return <p>読み込み中...</p>;
     if (error) return <p>エラー: {error.response?.data.error}</p>;
@@ -28,19 +38,21 @@ const GroupMembers = () => {
                 <div className={styles.members_wrap}>
                     {data?.members?.map((member) => (
                         <List
-                            name={member.username}
-                            icon={`${member.profile_image_url}`}
-                            key={`${member.username}-${member.role}`}
+                            addHost={addHost && member.username !== data.myName}
+                            member={member}
+                            key={`${member.id}-${member.role}`}
+                            admin={data.myRole === "admin" && member.username !== data.myName}
+                            onClick={handleSelectAddHost}
                         />
                     ))}
                 </div>
             </div>
-            <GroupSettings
-                groupId={groupId}
-                role={`${data?.MyRole}`}
-                icon_image_url={`${data?.group_icon}`}
-                name={`${data?.group_name}`}
-            />
+            {data?.myRole === "admin" && (
+                <SubmitBtn
+                    text={!addHost ? "ホスト権限を与える" : "完了"}
+                    onClick={() => setAddHost((prev) => !prev)}
+                />
+            )}
         </div>
     );
 };
