b40c2c973e62e3d3801347eb6fa59abefb34a7f5
sojiro takagi <soujirou.takagi@gmail.com>
2025-12-09 16:11:27 +0900

    #36 update:プロフィールアイコンを表示

--- src/components/ui/Navigation/Navigation.tsx
+++ src/components/ui/Navigation/Navigation.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useCurrentUser } from "@/features/auth/hooks/useCurrentUser";
 import styles from "@/components/ui/Navigation/Navigation.module.scss";
 import clsx from "clsx";
 
@@ -14,32 +15,34 @@ type NavsObject = {
 
 type Navs = NavsObject[];
 
-const Navs: Navs = [
-    {
-        text: "チャット",
-        href: "/chat",
-        icon: "/images/navigation/chat.svg",
-    },
-    {
-        text: "マップ",
-        href: "/map",
-        icon: "/images/navigation/map.svg",
-    },
-    {
-        text: "通知",
-        href: "/notice",
-        icon: "/images/navigation/notice.svg",
-    },
-    {
-        text: "プロフィール",
-        href: "/profile",
-        icon: "/images/navigation/chat.svg",
-    },
-];
-
 const Navigation = () => {
     const pathname = usePathname();
+    const { data } = useCurrentUser();
+    console.log(data);
+    const profile_icon = data?.user?.profile_image_url ?? "/images/groups/test_icon.webp";
 
+    const Navs: Navs = [
+        {
+            text: "チャット",
+            href: "/chat",
+            icon: "/images/navigation/chat.svg",
+        },
+        {
+            text: "マップ",
+            href: "/map",
+            icon: "/images/navigation/map.svg",
+        },
+        {
+            text: "通知",
+            href: "/notice",
+            icon: "/images/navigation/notice.svg",
+        },
+        {
+            text: "プロフィール",
+            href: "/profile",
+            icon: `${profile_icon}`,
+        },
+    ];
     return (
         <div className={styles.navWrap}>
             {Navs.map((nav) => (
