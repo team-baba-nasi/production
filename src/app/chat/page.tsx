a54c6c91d483627148dd8469f95490d882a19f46
sojiro takagi <soujirou.takagi@gmail.com>
2025-12-30 17:01:16 +0900

    #38 add:ダミーデータを用いたチャットリストページの作成

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,33 @@
-import Navigation from "@/components/ui/Navigation/Navigation";
+"use client";
 
-export default function Home() {
+import Navigation from "@/components/ui/Navigation/Navigation";
+import InputField from "@/components/ui/InputField/InputField";
+import styles from "@/features/chat/styles/pages/chatList.module.scss";
+import ChatGroup from "@/features/chat/components/ChatGroup";
+import { useState } from "react";
+
+const Home = () => {
+    const [searchGroupName, setSearchGroupName] = useState<string>("");
+
     return (
-        <>
-            <p>トップページ</p>
-            <Navigation />
-        </>
+        <div className={styles.pageWrap}>
+            <div className={styles.content}>
+                <h2 className="text-title">チャット</h2>
+                <Navigation />
+                <InputField
+                    placeholder="グループ名を検索"
+                    search
+                    edit
+                    value={searchGroupName}
+                    onChange={setSearchGroupName}
+                />
+                <ChatGroup />
+                <ChatGroup />
+                <ChatGroup />
+                <ChatGroup />
+            </div>
+        </div>
     );
-}
+};
+
+export default Home;
