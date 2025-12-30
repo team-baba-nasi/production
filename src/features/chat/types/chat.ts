bb2f5297c09ac20001bf4b762c7e1fbe6f5cdf0c
sojiro takagi <soujirou.takagi@gmail.com>
2025-12-30 18:50:51 +0900

    レスポンス型作成

---
+++ src/features/chat/types/chat.ts
@@ -0,0 +1,36 @@
+export type ChatGroupListResponse = {
+    chatGroups: ChatGroupItem[];
+};
+
+export type ChatGroupItem = {
+    chat_room: ChatRoom;
+};
+
+export type ChatRoom = {
+    id: number;
+    uuid: string;
+    room_type: string;
+    created_at: Date;
+    updated_at: Date;
+    participants: ChatParticipant[];
+    messages: ChatMessage[];
+};
+
+export type ChatParticipant = {
+    id: number;
+    user: {
+        id: number;
+        username: string;
+        profile_image_url: string | null;
+    };
+};
+
+export type ChatMessage = {
+    id: number;
+    content: string;
+    created_at: Date;
+    sender: {
+        id: number;
+        username: string;
+    };
+};
