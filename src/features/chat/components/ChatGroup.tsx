405f32f7e5e107921469d4f1740d69225a934264
sojiro takagi <soujirou.takagi@gmail.com>
2025-12-31 16:44:12 +0900

    グループ一覧をバックエンドと結合

--- src/features/chat/components/ChatGroup.tsx
+++ src/features/chat/components/ChatGroup.tsx
@@ -2,10 +2,16 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "@/features/chat/styles/ChatGroup.module.scss";
 import clsx from "clsx";
+import { ChatRoom, ChatMessage } from "../types/chat";
 
-const ChatGroup = () => {
+interface ChatGroupProos {
+    room: ChatRoom;
+    lastMessage: ChatMessage;
+}
+
+const ChatGroup = ({ room, lastMessage }: ChatGroupProos) => {
     return (
-        <Link href={`/chat/`} className={styles.wrap}>
+        <Link href={`/chat/${room.uuid}`} className={styles.wrap}>
             <Image
                 src={"/images/groups/test_icon.webp"}
                 alt="チャットグループアイコン"
@@ -14,10 +20,17 @@ const ChatGroup = () => {
             />
             <div>
                 <div className={styles.info}>
-                    <p className="text_normal bold">みんな仲良しグループ(4)</p>
-                    <p className={clsx("text_sub", styles.info, styles.subtext)}>11:21</p>
+                    <p className="text_normal bold">
+                        みんな仲良しグループ({room.participants.length})
+                    </p>
+                    <p className={clsx("text_sub", styles.info, styles.subtext)}>
+                        {new Date(lastMessage.created_at).toLocaleTimeString("ja-JP", {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                        })}
+                    </p>
                 </div>
-                <p className={clsx("text_sub", styles.subtext)}>ありがとう</p>
+                <p className={clsx("text_sub", styles.subtext)}>{lastMessage.content}</p>
             </div>
         </Link>
     );
