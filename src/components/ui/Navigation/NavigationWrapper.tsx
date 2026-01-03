8c03da69ff498663a9b79500c5bfb8be84a4de4e
sojiro takagi <soujirou.takagi@gmail.com>
2026-01-04 02:22:53 +0900

    #38 update:チャット画面ではナビゲーションを非表示に

--- src/components/ui/Navigation/NavigationWrapper.tsx
+++ src/components/ui/Navigation/NavigationWrapper.tsx
@@ -4,9 +4,16 @@ import { usePathname } from "next/navigation";
 import Navigation from "@/components/ui/Navigation/Navigation";
 
 const shouldHideNavigation = (pathname: string): boolean => {
+    // 認証系
     if (pathname.startsWith("/auth")) return true;
 
+    // グループ詳細
     if (pathname.startsWith("/groups/") && pathname !== "/groups") {
+        return true;
+    }
+
+    // チャット詳細
+    if (pathname.startsWith("/chat/") && pathname !== "/chat") {
         return true;
     }
 
