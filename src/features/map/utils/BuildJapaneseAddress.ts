4cc54fcc8d94e4f6d7ded5140d4d38acba0456f2
sojiro takagi <soujirou.takagi@gmail.com>
2026-01-05 19:23:42 +0900

    #42 update:utilityをより詳細に取得する様修正

--- src/features/map/utils/BuildJapaneseAddress.ts
+++ src/features/map/utils/BuildJapaneseAddress.ts
@@ -4,7 +4,14 @@ type JapaneseAddress = {
 };
 
 const buildJapaneseAddress = (place: google.maps.places.PlaceResult): JapaneseAddress => {
-    const components = place.address_components ?? [];
+    if (!place.address_components) {
+        return {
+            postalCode: undefined,
+            address: place.formatted_address?.replace(/^日本、\s?/, "") ?? "",
+        };
+    }
+
+    const components = place.address_components;
 
     const find = (type: string): string | undefined =>
         components.find((c) => c.types.includes(type))?.long_name;
@@ -13,10 +20,24 @@ const buildJapaneseAddress = (place: google.maps.places.PlaceResult): JapaneseAd
 
     const prefecture = find("administrative_area_level_1");
     const city = find("locality") ?? find("administrative_area_level_2");
-    const town = find("sublocality_level_1");
-    const street = find("route");
 
-    const address = [prefecture, city, town, street].filter(Boolean).join("");
+    const town = find("sublocality_level_1");
+
+    // 日本特有
+    const chome = find("sublocality_level_2"); // ○丁目
+    const ban = find("sublocality_level_3"); // ○番
+    const go = find("street_number") ?? find("premise"); // ○号
+
+    const addressParts = [
+        prefecture,
+        city,
+        town,
+        chome ? `${chome}` : undefined,
+        ban ? `${ban}` : undefined,
+        go ? `${go}` : undefined,
+    ];
+
+    const address = addressParts.filter(Boolean).join("");
 
     return {
         postalCode,
