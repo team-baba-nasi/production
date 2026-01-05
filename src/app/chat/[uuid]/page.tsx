47aab6d769359c93de94a5d905796edcea10efa8
sojiro takagi <soujirou.takagi@gmail.com>
2026-01-05 14:40:36 +0900

    #38 update:チャット入力欄、送信ボタンを追加

--- src/app/chat/[uuid]/page.tsx
+++ src/app/chat/[uuid]/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import clsx from "clsx";
 import { useParams } from "next/navigation";
+import { useState } from "react";
 import { useCurrentUser } from "@/features/auth/hooks/useCurrentUser";
 import styles from "@/features/chat/styles/pages/chat.module.scss";
 import { useChatMessages } from "@/features/chat/hooks/useChatMessages";
@@ -22,6 +23,10 @@ type ShopInfo = {
 };
 
 const Chat = () => {
+    const handleSubmitMessge = () => {
+        console.log(message);
+    };
+
     const shopInfo: ShopInfo = {
         name: "山根屋",
         address: "大阪府大阪市北区中崎西1丁目4−22",
@@ -33,6 +38,8 @@ const Chat = () => {
         },
         time: "17:00 ~ 19:00",
     };
+
+    const [message, setMessage] = useState<string>("");
 
     const { uuid } = useParams<{ uuid: string }>();
 
@@ -49,88 +56,107 @@ const Chat = () => {
     }
 
     return (
-        <div className={styles.wrap}>
-            {/* header */}
-            <div className={styles.header}>
-                <Link href="/chat">
+        <>
+            <div className={styles.wrap}>
+                {/* header */}
+                <div className={styles.header}>
+                    <Link href="/chat">
+                        <Image
+                            src="/images/ui/arrow_back.svg"
+                            alt="backToPageButton"
+                            width={24}
+                            height={24}
+                        />
+                    </Link>
+
+                    <div className={styles.groupInfo}>
+                        <Image
+                            src="/images/groups/test_icon.webp"
+                            alt="グループアイコン"
+                            width={32}
+                            height={32}
+                        />
+                        <h2 className="text_normal bold">チャット</h2>
+                    </div>
+                    <div />
+                </div>
+
+                <div className={styles.shopInfoWrap}>
+                    <div className={styles.matching}>
+                        <p className="text_normal bold">マッチングしました</p>
+                    </div>
+
+                    <div className={styles.shopInfo}>
+                        <Image
+                            src="/images/groups/test_icon_4x.webp"
+                            alt="ショッププレビュー"
+                            width={108}
+                            height={108}
+                        />
+
+                        <div className={styles.shopTexts}>
+                            <div>
+                                <h3 className="text_normal">{shopInfo.name}</h3>
+                                <p className="text_sub">{shopInfo.address}</p>
+                            </div>
+
+                            <div>
+                                <div className={styles.messageTime}>
+                                    <p>
+                                        {shopInfo.date.year}
+                                        <span className="text_sub">/</span>
+                                        {shopInfo.date.month}
+                                        <span className="text_sub">/</span>
+                                        {shopInfo.date.day}
+                                    </p>
+
+                                    <div className={styles.weekDayWrap}>
+                                        <Image
+                                            src="/images/chat/eclipse.svg"
+                                            alt="曜日の背景の円"
+                                            width={24}
+                                            height={24}
+                                        />
+                                        <p className={clsx(styles.weekDay, "text_sub bold")}>
+                                            {shopInfo.date.weekday}
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <p className="text_sub bold">{shopInfo.time}</p>
+                            </div>
+                        </div>
+                    </div>
+
                     <Image
-                        src="/images/ui/arrow_back.svg"
-                        alt="backToPageButton"
+                        src="/images/chat/cracker.png"
+                        alt="クラッカー"
+                        width={64}
+                        height={64}
+                        className={styles.cracker}
+                    />
+                </div>
+
+                <ChatMessages chatRoom={data.chatRoom} myUserId={currentUser.user?.id} />
+            </div>
+            <div className={styles.inputContainer}>
+                <input
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className="text_normal"
+                    placeholder="メッセージを入力"
+                />
+                <div className={styles.submitBtn} onClick={handleSubmitMessge}>
+                    <Image
+                        src="/images/chat/send.svg"
+                        alt="メッセージ送信ボタン"
                         width={24}
                         height={24}
                     />
-                </Link>
-
-                <div className={styles.groupInfo}>
-                    <Image
-                        src="/images/groups/test_icon.webp"
-                        alt="グループアイコン"
-                        width={32}
-                        height={32}
-                    />
-                    <h2 className="text_normal bold">チャット</h2>
                 </div>
-                <div />
             </div>
-
-            <div className={styles.shopInfoWrap}>
-                <div className={styles.matching}>
-                    <p className="text_normal bold">マッチングしました</p>
-                </div>
-
-                <div className={styles.shopInfo}>
-                    <Image
-                        src="/images/groups/test_icon_4x.webp"
-                        alt="ショッププレビュー"
-                        width={108}
-                        height={108}
-                    />
-
-                    <div className={styles.shopTexts}>
-                        <div>
-                            <h3 className="text_normal">{shopInfo.name}</h3>
-                            <p className="text_sub">{shopInfo.address}</p>
-                        </div>
-
-                        <div>
-                            <div className={styles.messageTime}>
-                                <p>
-                                    {shopInfo.date.year}
-                                    <span className="text_sub">/</span>
-                                    {shopInfo.date.month}
-                                    <span className="text_sub">/</span>
-                                    {shopInfo.date.day}
-                                </p>
-
-                                <div className={styles.weekDayWrap}>
-                                    <Image
-                                        src="/images/chat/eclipse.svg"
-                                        alt="曜日の背景の円"
-                                        width={24}
-                                        height={24}
-                                    />
-                                    <p className={clsx(styles.weekDay, "text_sub bold")}>
-                                        {shopInfo.date.weekday}
-                                    </p>
-                                </div>
-                            </div>
-
-                            <p className="text_sub bold">{shopInfo.time}</p>
-                        </div>
-                    </div>
-                </div>
-
-                <Image
-                    src="/images/chat/cracker.png"
-                    alt="クラッカー"
-                    width={64}
-                    height={64}
-                    className={styles.cracker}
-                />
-            </div>
-
-            <ChatMessages chatRoom={data.chatRoom} myUserId={currentUser.user?.id} />
-        </div>
+        </>
     );
 };
 
