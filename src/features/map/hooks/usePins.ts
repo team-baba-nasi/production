933db013b2d46494058292e6c5aacecadc63123f
sojiro takagi <soujirou.takagi@gmail.com>
2026-01-06 18:17:14 +0900

    #42 update:Route変更に伴い不要なものを削除

--- src/features/map/hooks/usePins.ts
+++ src/features/map/hooks/usePins.ts
@@ -6,29 +6,20 @@ import { AxiosError } from "axios";
 import { GetPinsError, GetPinsResponse } from "../types/map";
 
 type UsePinsOptions = {
-    scope?: "mine";
-    groupId?: number;
+    groupId: number;
 };
 
-export function usePins(options?: UsePinsOptions) {
-    const params = new URLSearchParams();
-
-    if (options?.scope) {
-        params.append("scope", options.scope);
-    }
-
-    if (options?.groupId !== undefined) {
-        params.append("group_id", options.groupId.toString());
-    }
-
-    const queryString = params.toString();
-    const url = queryString ? `/maps/pins?${queryString}` : "/maps/pins";
-
+export function usePins({ groupId }: UsePinsOptions) {
     return useQuery<GetPinsResponse, AxiosError<GetPinsError>>({
-        queryKey: ["pins", options?.scope, options?.groupId],
+        queryKey: ["pins", groupId],
         queryFn: async () => {
-            const res = await axios.get(url);
+            const res = await axios.get("/maps/pins", {
+                params: {
+                    group_id: groupId,
+                },
+            });
             return res.data;
         },
+        enabled: Number.isFinite(groupId),
     });
 }
